cfd4d5ea3f6bf018207cc84379bea26c242945b7
RayanDiniz <rayancassiokiol@gmail.com>
2021-04-07 22:30:49 -0300

    atualização

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,63 +1,62 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, {useState} from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { Container, Row, Col, Image, Button, Form } from 'react-bootstrap';
 import { FaArrowLeft } from "react-icons/fa";
-import { Transition } from "react-transition-group";
 
+import api from '../../services/api';
 import "./Styles.css";
 import Logi from "../../assets/login-illustration.jpg";
 
-const duration = 300;
+function Login() {
+  const [email, setEmail] = useState('');
+  const history = useHistory();
 
-const defaultStyle = {
-  transition: `opacity ${duration}ms ease-in-out`,
-  opacity: 1,
+  async function handleLogin(e) {
+      e.preventDefault();
+      
+      try {
+          const response = await api.post('sessions', { email });
+
+          localStorage.setItem('userEmail', email);
+          localStorage.setItem('userName', response.data.name);
+
+          history.push('/profile');
+      } catch (err) {
+          alert('Falha no login, tente novamente');
+      }
+  }
+
+  return (
+    <Container>
+      <Row>
+        <Col className="linklogin">
+          <Link to="/" className="btnLink">
+            <FaArrowLeft />
+        voltar
+      </Link>
+        </Col>
+      </Row>
+
+      <Row className="formCenter">
+        <Form onSubmit={handleLogin} className="form">
+          <Image src={Logi} fluid rounded alt="login" />
+          <input type="email" value={email} placeholder="Login" />
+          <input type="password" placeholder="Senha" />
+
+          <Form.Row className="formCenter">
+            <Button type="submit" variant="danger" size="lg">Entrar</Button>
+          </Form.Row>
+
+          <Link to="/cadastro" className="btnLink">Cadastre-se</Link>
+        </Form>
+      </Row>
+
+      <Row>
+        <Col className="rdiniz">&copy;RayanDiniz</Col>
+      </Row>
+
+    </Container>
+  );
 }
-
-const transitionStyles = {
-  entering: { opacity: 1 },
-  entered: { opacity: 1 },
-  exiting: { opacity: 1 },
-  exited: { opacity: 1 },
-};
-const Login = ({ in: inProp }) => (
-
-  <Transition in={inProp} timeout={duration}>
-    {state => (
-      <Container style={{
-        ...defaultStyle,
-        ...transitionStyles[state]
-      }}>
-        <Row>
-          <Col className="linklogin">
-            <Link to="/" className="btnLink">
-              <FaArrowLeft />
-            voltar
-          </Link>
-          </Col>
-        </Row>
-
-        <Row className="formCenter">
-          <Form className="form">
-            <Image src={Logi} fluid rounded alt="login" />
-            <input type="email" placeholder="Login" />
-            <input type="password" placeholder="Senha" />
-
-            <Form.Row className="formCenter">
-              <Button type="submit" variant="danger" size="lg">Entrar</Button>
-            </Form.Row>
-
-            <Link to="/cadastro" className="btnLink">Cadastre-se</Link>
-          </Form>
-        </Row>
-
-        <Row>
-          <Col className="rdiniz">&copy;RayanDiniz</Col>
-        </Row>
-
-      </Container>
-    )}
-  </Transition>
-);
 
 export default Login;
