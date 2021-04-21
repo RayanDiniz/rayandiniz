647aca4a2723e609bc930c1a51a21b2fe9e5fdb8
RayanDiniz <rayancassiokiol@gmail.com>
2021-04-21 16:10:39 -0300

    api

---
+++ api/v1/classes/Servico.php
@@ -0,0 +1,20 @@
+<?php
+class Servico {
+    public function mostrar() {
+        $con = new PDO('mysql: host=localhost; dbname=zinid', 'root', '');
+
+        $sql = "SELECT * FROM servicos ORDER BY id ASC";
+        $sql = $con->prepare($sql);
+        $sql->execute();
+
+        $resultados = array();
+
+        while($row = $sql->fetch(PDO::FETCH_ASSOC)) {
+            $resultados[] = $row;
+        }
+        if (!$resultados) {
+            throw new Exception("Nenhum serviço ainda");
+        }
+        return $resultados;
+    }
+}
