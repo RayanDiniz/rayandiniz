3b79b4722cb512d6fb4a991af4783bb4bf9c1a9e
rayandiniz <rayancassiokiol@gmail.com>
2022-06-29 14:55:08 -0300

    Page About

--- src/components/Sobre.tsx
+++ src/components/Sobre.tsx
@@ -2,25 +2,28 @@ import * as React from "react";
 const Sobre = () => {
   return (
     <div className="AppSobre container">
-      <p className="m-3">
-        Analista e desenvolvedor de sistemas especializado em
+      <h1>Seja bem-vindo!</h1>
+      <p>
+        Eu sou o
+        Rayan Cassio Vieira Diniz, e quero que você seja muito bem-vindo 
+        a minha homepage. Aqui você encotra meus contatos, minha formação, meu potifolio, minhas experiencias e mais informações. Fique a vontade!
+      </p>
+      <p>
+        Sou analista e desenvolvedor de sistemas especializado em
         Segurança da Informação. Atualmente possuo uma startup que
         constrói aplicações e sistemas para outras empresas, também
         sou proprietário de uma sorveteria da família naqual se
         encontrava desativada. Procuro novos desafios na carreira de
         tecnologia.
-      </p><h4>Seja bem-vindo!</h4>
-        <p>Olá! Eu sou o
-          Rayan Cassio Vieira Diniz, e quero que você seja muito bem-vindo a minha homepage. Aqui em baixo você encotra meus contatos e nas abas ao lado tem mais informações, fique a vontade.</p>
-
-        <span>Analista e Desenvolvedor de Sistemas</span>
-        <div className="Stack">
-          <div className="primary">PHP</div>
-          <div className="warning">JavaScript</div>
-          <div className="success">Python</div>
-          <div className="danger">HTML</div>
-          <div className="info">CSS</div>
-        </div>
+      </p>
+      <h2>Stack</h2>
+      <div className="stacks">
+        <div className="pill primary">PHP</div>
+        <div className="pill warning">JavaScript</div>
+        <div className="pill success">Python</div>
+        <div className="pill danger">HTML</div>
+        <div className="pill info">CSS</div>
+      </div>
     </div>
   );
 }
