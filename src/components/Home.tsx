6f1b94bea184a7434944570f318456de1c152148
rayandiniz <rayancassiokiol@gmail.com>
2022-06-29 01:45:37 -0300

    nova aparencia

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,20 +1,16 @@
 import * as React from "react";
 import Typist from "react-typist";
-import TypistLoop from "react-typist-loop";
 const Home = () => {
   return (
     <div className="AppHome container">
       <div id="mascara">
-        <TypistLoop interval={3000}>
-        {["a brazilian full stack developer."].map(text =>
-          <Typist key={text} startDelay={1000} className="boasVindasText">
-            <p>Hi!</p>
-            I'm Rayan Diniz
-            <Typist.Backspace count={11} />
-            {text}
-          </Typist>
-        )}
-        </TypistLoop>
+
+        <Typist startDelay={1000} className="boasVindasText">
+          <p>Hi!</p>
+          I'm Rayan Diniz
+          <Typist.Backspace count={11} />
+          a brazilian full stack developer.
+        </Typist>
       </div>
     </div >
   )
